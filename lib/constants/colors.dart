5905da6e32374265d1656ce0d6e193dc3b477109
gan3sh09 <98739865+gan3sh09@users.noreply.github.com>
2024-05-11 15:49:38 +0545

    Fitpal

---
+++ lib/constants/colors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+/* LIST OF COLORS */
+
+const primaryColor = Color(0xFF673AB7);
+const secondaryColor = Color(0xFF272727);
+const accentColor = Color(0xFF001BFF);
+
+const cardBgColor = Color(0xFFF7F6F1);
+const whiteColor = Color(0xffffffff);
+const darkColor = Color(0xFF000000);
+
+// Color for search box
+const ssecondaryColor = Color(0xFF979797);
